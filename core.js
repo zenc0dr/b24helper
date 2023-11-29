c953c3b792012f814bfbec7ea234ebdf378ed2bf
Alex Blaze <zen@8ber.ru>
2023-11-29 10:23:13 +0400

    Подстроился под новый рефакторинг b24

--- core.js
+++ core.js
@@ -1,23 +1,25 @@
 let trashPhrases = [
     'Крайний срок:',
     'ваша эффективность будет снижена.',
-    'Завершите задачу или передвиньте срок.'
+    'Завершите задачу или передвиньте срок.',
+    'Отреагировал'
 ]
 
-$(document).on('click', '.bx-im-notifications-header-image', () => {
+$(document).on('click', '.bx-im-content-notification__panel-title_icon', () => {
     let ids = []
     let interval = 0
     console.log('kill tasks')
-    $('.bx-im-notifications-item').each((index, item) => {
+    $('.bx-im-content-notification-item__content-container').each((index, item) => {
         let $item = $(item)
-        let url = $item.find('.bx-im-notifications-item-link').attr('href')
+        let url = $item.find('.bx-im-content-notification-item-content__content-text a').eq(0).attr('href')
         let taskId = /view\/(\d+)\//.exec(url)[1]
-        let subtitleText = $item.find('.bx-im-notifications-item-bottom-no-subtitle-text').text()
+        console.log('task_id=' + taskId)
+        let subtitleText = $item.find('.bx-im-content-notification-item-content__content-text').text()
         let isTrash = trashPhrases.some(phrase => subtitleText.includes(phrase))
         if (ids.includes(taskId) || isTrash) {
             $item.css('background', 'red')
             setTimeout(() => {
-                $item.find('.bx-im-notifications-item-header-delete').click()
+                $item.find('.bx-im-content-notification-item-header__delete-button').click()
             }, interval)
             interval += 200
         }
