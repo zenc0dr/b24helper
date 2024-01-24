6cc127978a0bea2697caa6062fd8bc4af0fbb1bf
Alex Blaze <zen@8ber.ru>
2024-01-24 10:59:09 +0400

    Some fixes

--- core.js
+++ core.js
@@ -5,6 +5,17 @@ let trashPhrases = [
     'Отреагировал'
 ]
 
+function writeCounter()
+{
+    let tasks_qnt = $('.bx-im-content-notification-item__container').length
+    let counter_exists = $('.bx-im-content-notification__panel_text .zen-counter').length
+    if (!counter_exists) {
+        $('.bx-im-content-notification__panel_text').append('<div class="zen-counter">' + tasks_qnt + ' шт.</div>')
+    } else {
+        $('.bx-im-content-notification__panel_text .zen-counter').text(tasks_qnt + ' шт.')
+    }
+}
+
 $(document).on('click', '.bx-im-content-notification__panel-title_icon', () => {
     let ids = []
     let interval = 0
@@ -14,8 +25,12 @@ $(document).on('click', '.bx-im-content-notification__panel-title_icon', () => {
         let url = $item.find('.bx-im-content-notification-item-content__content-text a').eq(0).attr('href')
         let taskId = /view\/(\d+)\//.exec(url)[1]
         console.log('task_id=' + taskId)
-        let subtitleText = $item.find('.bx-im-content-notification-item-content__content-text').text()
-        let isTrash = trashPhrases.some(phrase => subtitleText.includes(phrase))
+
+        // Поиск мусора может удалять нужные уведомления
+        //let subtitleText = $item.find('.bx-im-content-notification-item-content__content-text').text()
+        //let isTrash = trashPhrases.some(phrase => subtitleText.includes(phrase))
+        let isTrash = false
+
         if (ids.includes(taskId) || isTrash) {
             $item.css('background', 'red')
             setTimeout(() => {
@@ -24,5 +39,7 @@ $(document).on('click', '.bx-im-content-notification__panel-title_icon', () => {
             interval += 200
         }
         ids.push(taskId)
+        writeCounter()
     })
+    writeCounter()
 })
